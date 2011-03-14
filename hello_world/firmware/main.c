c40c885a783a8f23963ee09ee2d9392c75205614
Mika Tuupola <tuupola@appelsiini.net>
2011-03-14 21:16:49 +0200

    Update header.

--- hello_world/firmware/main.c
+++ hello_world/firmware/main.c
@@ -1,7 +1,19 @@
+/*
+ * Hello world of AVR. Blink led PORTB5 (Arduino digital 13).
+ *
+ * To compile and upload run: make clean; make; make program;
+ *
+ * Copyright 2009-2011 Mika Tuupola
+ *
+ * Licensed under the MIT license:
+ *   http://www.opensource.org/licenses/mit-license.php
+ *
+ */
+ 
 #include <avr/io.h>
 #include <util/delay.h>
 
-int main(void)
+int main(void) 
 {
     /* Make PORTB5 (Arduino pin 13) an output. */
     DDRB |= _BV(PORTB5);
