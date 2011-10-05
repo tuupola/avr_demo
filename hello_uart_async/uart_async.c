55056f94f0c8fa27cdd9927dcd0adb29341e9474
Mika Tuupola <tuupola@appelsiini.net>
2011-10-05 23:43:17 +0300

    Whitespace.

--- hello_uart_async/uart_async.c
+++ hello_uart_async/uart_async.c
@@ -16,15 +16,15 @@
 #endif
 
 struct tx_ring {
-	int buffer[UART_TX_BUFFER_SIZE];
-	int start;
-	int end;
+    int buffer[UART_TX_BUFFER_SIZE];
+    int start;
+    int end;
 };
 
 struct rx_ring {
-	int buffer[UART_RX_BUFFER_SIZE];
-	int start;
-	int end;
+    int buffer[UART_RX_BUFFER_SIZE];
+    int start;
+    int end;
 };
 
 static struct tx_ring tx_buffer;
@@ -46,48 +46,48 @@ int uart_putchar(char c, FILE *stream) {
     if (c == '\n') {
         uart_putchar('\r', stream);
     }
-	
-	int write_pointer = (tx_buffer.end + 1) % UART_TX_BUFFER_SIZE;
-	
-	if (write_pointer != tx_buffer.start){
-		tx_buffer.buffer[tx_buffer.end] = c;
-		tx_buffer.end = write_pointer;
-		
-		/* Data available. Enable the transmit interrupt for serial port 0. */
-		UCSR0B |= _BV(UDRIE0);	
-	}
-	
+    
+    int write_pointer = (tx_buffer.end + 1) % UART_TX_BUFFER_SIZE;
+    
+    if (write_pointer != tx_buffer.start){
+        tx_buffer.buffer[tx_buffer.end] = c;
+        tx_buffer.end = write_pointer;
+        
+        /* Data available. Enable the transmit interrupt for serial port 0. */
+        UCSR0B |= _BV(UDRIE0);  
+    }
+    
     return 0;
 }
 
 int uart_getchar(FILE *stream) {
     int read_pointer = (rx_buffer.start + 1) % UART_RX_BUFFER_SIZE;
-	
-	rx_buffer.start = read_pointer;
-	return rx_buffer.buffer[read_pointer];
+    
+    rx_buffer.start = read_pointer;
+    return rx_buffer.buffer[read_pointer];
 }
 
 ISR(USART_RX_vect) {
-	int write_pointer = (rx_buffer.end + 1) % UART_RX_BUFFER_SIZE;
+    int write_pointer = (rx_buffer.end + 1) % UART_RX_BUFFER_SIZE;
 
     /* Add next byte to ringbuffer if it has space available. */
-	if (write_pointer != rx_buffer.start){
-		rx_buffer.buffer[rx_buffer.end] = UDR0;
-		rx_buffer.end = write_pointer;
-	}
+    if (write_pointer != rx_buffer.start){
+        rx_buffer.buffer[rx_buffer.end] = UDR0;
+        rx_buffer.end = write_pointer;
+    }
 }
 
 ISR(USART_UDRE_vect){
-	int read_pointer = (tx_buffer.start + 1) % UART_TX_BUFFER_SIZE;
-	
+    int read_pointer = (tx_buffer.start + 1) % UART_TX_BUFFER_SIZE;
+    
     /* Transit next byte if data available in ringbuffer. */
-	if (read_pointer != tx_buffer.end) {
-		UDR0 = tx_buffer.buffer[read_pointer];
-		tx_buffer.start = read_pointer;
-	} else {
-	    /* Nothing to send. Disable the transmit interrupt for serial port 0. */
-		UCSR0B &= ~_BV(UDRIE0);
-	}
+    if (read_pointer != tx_buffer.end) {
+        UDR0 = tx_buffer.buffer[read_pointer];
+        tx_buffer.start = read_pointer;
+    } else {
+        /* Nothing to send. Disable the transmit interrupt for serial port 0. */
+        UCSR0B &= ~_BV(UDRIE0);
+    }
 }
 
 
