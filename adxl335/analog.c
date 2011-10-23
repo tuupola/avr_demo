3d00faf6390b9a1a7b50bfbd3ed79befa33a5df0
Mika Tuupola <tuupola@appelsiini.net>
2011-10-23 17:26:05 +0300

    Tabs to spaces.

--- adxl335/analog.c
+++ adxl335/analog.c
@@ -16,46 +16,46 @@
 #include "analog.h"
 
 uint16_t analog_read(uint8_t pin) {
-	
-	/* Enable ADC and set prescaler. */
-	ADCSRA = _BV(ADEN) | ADC_PRESCALER;
-	
-	/* Set high speed mode. */
+    
+    /* Enable ADC and set prescaler. */
+    ADCSRA = _BV(ADEN) | ADC_PRESCALER;
+    
+    /* Set high speed mode. */
     ADCSRB |=  _BV(ADHSM);
-	
-	/* Set adc reference and select mux. */
-	ADMUX = ADC_REF | (pin & 0b00011111);
-	
-	/* Start conversion. */
+    
+    /* Set adc reference and select mux. */
+    ADMUX = ADC_REF | (pin & 0b00011111);
+    
+    /* Start conversion. */
     ADCSRA |= _BV(ADSC);
     
     /* Wait until adc has result ready. */
     loop_until_bit_is_clear(ADCSRA, ADSC);
 
-	/* Word! */
-	return ADCW;
+    /* Word! */
+    return ADCW;
 }
 
 uint8_t analog_read_byte(uint8_t pin) {
-	
-	/* Enable ADC and set prescaler. */
-	ADCSRA = _BV(ADEN) | ADC_PRESCALER;
-	
-	/* Set high speed mode. */
+    
+    /* Enable ADC and set prescaler. */
+    ADCSRA = _BV(ADEN) | ADC_PRESCALER;
+    
+    /* Set high speed mode. */
     ADCSRB |=  _BV(ADHSM);
-	
-	/* Set adc reference and select mux. */
-	ADMUX = ADC_REF | (pin & 0b00011111);
-	
-	/* Left adjust the 10 bit result. */
+    
+    /* Set adc reference and select mux. */
+    ADMUX = ADC_REF | (pin & 0b00011111);
+    
+    /* Left adjust the 10 bit result. */
     ADMUX  |=  (1<<ADLAR);
-	
-	/* Start conversion. */
+    
+    /* Start conversion. */
     ADCSRA |= _BV(ADSC);
     
     /* Wait until adc has result ready. */
     loop_until_bit_is_clear(ADCSRA, ADSC);
 
-	/* Hight byte. */
-	return ADCH;
+    /* Hight byte. */
+    return ADCH;
 }
